b9639757f842cf5bee72f615412092778c5342ec
redebron20 <redebron20@gmail.com>
2021-02-10 23:25:38 -0500

    Done.

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,11 @@
-class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
+class CreateLandmarks < ActiveRecord::Migration[5.2]
+  
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+      t.integer :figure_id
+    end
+  end
+
 end
---
+++ db/migrate/02_create_figures.rb
@@ -0,0 +1,10 @@
+class CreateFigures < ActiveRecord::Migration[5.2]
+    
+    def change
+      create_table :figures do |t|
+        t.string :name
+      end
+    end
+  
+  end
+  
